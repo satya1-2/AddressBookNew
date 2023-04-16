eb1854d651330395f57c4eac4fd7563a2f36adca
satya <satya143@gmail.com>
2023-04-17 00:48:43 +0530

    Add :Adding another contacts in addrssbook

--- AddressBookNew/src/main/java/com/bridgelabz/addressBook/AddressBookSystem.java
+++ AddressBookNew/src/main/java/com/bridgelabz/addressBook/AddressBookSystem.java
@@ -12,44 +12,53 @@ import java.util.Scanner;
         List<Contacts> Contacts = new ArrayList<Contacts>();
 
         public void addContacts() {
+            boolean addingContacts = true;
+            while (addingContacts) {
+                Contacts contacts = new Contacts();
+                System.out.println("Enter First Name : ");
+                String firstName = scanner.nextLine();
+                contacts.setFirstName(firstName);
 
-            System.out.println("Enter First Name : ");
-            String firstName = scanner.nextLine();
-            contacts.setFirstName(firstName);
+                System.out.println("Enter Last Name : ");
+                String lastName = scanner.nextLine();
+                contacts.setLastName(lastName);
 
-            System.out.println("Enter Last Name : ");
-            String lastName = scanner.nextLine();
-            contacts.setLastName(lastName);
+                System.out.println("Enter Address :");
+                String address = scanner.nextLine();
+                contacts.setAddress(address);
 
-            System.out.println("Enter Address :");
-            String address = scanner.nextLine();
-            contacts.setAddress(address);
+                System.out.println("Enter City : ");
+                String city = scanner.nextLine();
+                contacts.setCity(city);
 
-            System.out.println("Enter City : ");
-            String city = scanner.nextLine();
-            contacts.setCity(city);
+                System.out.println("Enter State : ");
+                String state = scanner.nextLine();
+                contacts.setState(state);
 
-            System.out.println("Enter State : ");
-            String state = scanner.nextLine();
-            contacts.setState(state);
+                System.out.println("Enter Email : ");
+                String email = scanner.nextLine();
+                contacts.setEmail(email);
 
-            System.out.println("Enter Email : ");
-            String email = scanner.nextLine();
-            contacts.setEmail(email);
+                System.out.println("Enter ZIP : ");
+                int zip = scanner.nextInt();
+                scanner.nextLine(); // consume the newline character
+                contacts.setZip(zip);
 
-            System.out.println("Enter ZIP : ");
-            int zip = scanner.nextInt();
-            contacts.setZip(zip);
+                System.out.println("Enter Phone Number");
+                long phoneNumber = scanner.nextLong();
+                scanner.nextLine(); // consume the newline character
+                contacts.setPhoneNumber(phoneNumber);
 
-            System.out.println("Enter Phone Number");
-            long phoneNumber = scanner.nextLong();
-            contacts.setPhoneNumber(phoneNumber);
-
-            Contacts.add(contacts);
-            System.out.println("Person Information has created sucessfully");
+                Contacts.add(contacts);
+                System.out.println("Person Information has been added successfully");
+                System.out.println("Do you want to add another person? (Y/N)");
+                String choice = scanner.nextLine();
+                if (choice.equalsIgnoreCase("N")) {
+                    addingContacts = false;
+                }
+            }
 
         }
-
         public void displayContacts() {
 
             if (Contacts.isEmpty()) {
