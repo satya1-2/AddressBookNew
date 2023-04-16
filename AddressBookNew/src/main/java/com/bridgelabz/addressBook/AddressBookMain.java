3af09e764cdea9e01416440e93fd4cae41ca5e85
satya <satya143@gmail.com>
2023-04-17 00:10:53 +0530

    Add:adding another contactbook in addressbook

--- AddressBookNew/src/main/java/com/bridgelabz/addressBook/AddressBookMain.java
+++ AddressBookNew/src/main/java/com/bridgelabz/addressBook/AddressBookMain.java
@@ -2,6 +2,12 @@ package com.bridgelabz.addressBook;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("welcome to addressbook ");
+
+            System.out.println("Welcome to Address Book System Program");
+
+            AddressBookSystem addressBookSystem = new AddressBookSystem();
+            addressBookSystem.addContacts();
+            addressBookSystem.displayContacts();
+        }
+
     }
-}
